558b251fc3891a558725a87689e26c8186392e8d
1F10220082 <s1f102200828@iniad.org>
2024-04-04 20:28:18 +0900

    カレンダーの対象月を変更できるように

--- task_calendar/urls.py
+++ task_calendar/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("calendar_main/", views.calendar_main, name="calendar_main"),
+    path("get_calendar/<str:year>/<str:month>/", views.get_calendar, name="get_calendar")
 ]
--- task_calendar/views.py
+++ task_calendar/views.py
@@ -9,8 +9,21 @@ def calendar_main(request):
     contexts = collect_regnum(request)
     year = datetime.datetime.now().year
     month = datetime.datetime.now().month
+    calendar.setfirstweekday(calendar.SUNDAY)
     list_calendar = calendar.monthcalendar(year, month)
     contexts["list_calendar"] = list_calendar
+    today = datetime.datetime.now().day
+    contexts["def_year"] = year
+    contexts["def_month"] = month
+    contexts["today"] = today
     return render(request, "task_calendar/main.html", contexts)
 
 
+def get_calendar(request, year, month):
+    contexts = collect_regnum(request)
+    calendar.setfirstweekday(calendar.SUNDAY)
+    list_calendar = calendar.monthcalendar(int(year), int(month))
+    contexts["list_calendar"] = list_calendar
+    if year == str(datetime.datetime.now().year) and month == str(datetime.datetime.now().month):
+        contexts["today"] = datetime.datetime.now().day
+    return render(request, "task_calendar/calendar_base.html", contexts)

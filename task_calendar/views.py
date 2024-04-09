44458729346b7ed6d6720ff3b2dc767dc1966144
1F10220082 <s1f102200828@iniad.org>
2024-04-09 11:50:59 +0900

    カレンダー機能の修正 ・テーブルの内容をⅠつのファイルに統一 ・読み込み状況を表示

--- task_calendar/views.py
+++ task_calendar/views.py
@@ -15,28 +15,21 @@ def calendar_main(request):
     contexts = collect_regnum(request)
     year = timezone.make_naive(timezone.now()).year
     month = timezone.make_naive(timezone.now()).month
-    calendar.setfirstweekday(calendar.SUNDAY)
-    list_calendar = calendar.monthcalendar(year, month)
-    contexts["list_calendar"] = list_calendar
     today = timezone.make_naive(timezone.now()).day
     contexts["def_year"] = year
     contexts["def_month"] = month
     contexts["today"] = today
-    contexts["list_interview"] = get_listInterview(request, year, month)
-    print(get_listInterview(request, year, month))
     return render(request, "task_calendar/main.html", contexts)
 
 
 def get_calendar(request, year, month):
-    contexts = collect_regnum(request)
+    contexts = {}
     calendar.setfirstweekday(calendar.SUNDAY)
     list_calendar = calendar.monthcalendar(int(year), int(month))
     contexts["list_calendar"] = list_calendar
     contexts["list_interview"] = get_listInterview(request, year, month)
     if year == str(timezone.make_naive(timezone.now()).year) and month == str(timezone.make_naive(timezone.now()).month):
         contexts["today"] = timezone.make_naive(timezone.now()).day
-    print(get_listInterview(request, year, month))
-    print(timezone.make_naive(timezone.now()))
     return render(request, "task_calendar/calendar_base.html", contexts)
 
 

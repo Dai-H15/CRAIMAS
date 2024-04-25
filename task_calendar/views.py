b6abd27ee8721e1782f202d0419dc1367a33b6f0
1F10220082 <s1f102200828@iniad.org>
2024-04-25 23:55:22 +0900

    #20への対応と軽微な修正

--- task_calendar/views.py
+++ task_calendar/views.py
@@ -9,7 +9,7 @@ from main.models import Interview
 
 def get_listInterview(request, year, month):
 
-    return [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in Interview.objects.filter(by_U_ID=request.user.U_ID).filter(date__year=year).filter(date__month=month)]
+    return [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in Interview.objects.filter(by_U_ID=request.user.U_ID).filter(date__year=year).filter(date__month=month).filter(RegistID__isActive=True)]
 
 
 def calendar_main(request):

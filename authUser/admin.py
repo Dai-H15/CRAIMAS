cbdfb4506af20a0a2a4f197b60e3eea4b2fe1594
1F10220082 <s1f102200828@iniad.org>
2024-05-28 14:37:42 +0900

    Adminにて管理用の機能を実装

--- authUser/admin.py
+++ authUser/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from .models import CustomUser
 
 # Register your models here.
-CustomUser = get_user_model()
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ("is_active", "username", "U_ID", "y_graduation", "is_staff", "is_superuser", "date_joined")
+    list_display_links = ("username", "U_ID")
+    search_fields = ["username", "email", "U_ID"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

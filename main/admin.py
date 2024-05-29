3ef8cc706a2a9653d3fcf4a04b9179ba650beb8f
1F10220082 <s1f102200828@iniad.org>
2024-05-29 13:34:33 +0900

    登録情報シートの可用性の向上

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import Companies, About, Idea, Motivation, D_Company, Adoption, RegistSets, Interview, Interviewer
 # Register your models here.
 
+
+class RegistSetsAdmin(admin.ModelAdmin):
+    model = RegistSets
+    list_display = ("company", "by_U_ID", "created")
+    search_fields = ["company", "by_U_ID"]
+
+
 admin.site.register(Companies)
 admin.site.register(About)
 admin.site.register(Idea)
 admin.site.register(Motivation)
 admin.site.register(D_Company)
 admin.site.register(Adoption)
-admin.site.register(RegistSets)
+admin.site.register(RegistSets, RegistSetsAdmin)
 admin.site.register(Interview)
 admin.site.register(Interviewer)

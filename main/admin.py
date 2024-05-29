6caa9f19fefdd1bc81692949b0dd5da0dcf9beaf
1F10220082 <s1f102200828@iniad.org>
2024-05-29 19:24:56 +0900

    修正

--- main/admin.py
+++ main/admin.py
@@ -5,16 +5,20 @@ from .models import Companies, About, Idea, Motivation, D_Company, Adoption, Reg
 
 class RegistSetsAdmin(admin.ModelAdmin):
     model = RegistSets
-    list_display = ("company__name", "by_U_ID", "created")
+    list_display = ("get_company_name", "by_U_ID", "created")
     search_fields = ["company__name", "by_U_ID"]
 
+    def get_company_name(self, obj):
+        return obj.company.name
+    get_company_name.short_description = 'Company Name'
 
+
+admin.site.register(RegistSets, RegistSetsAdmin)
 admin.site.register(Companies)
 admin.site.register(About)
 admin.site.register(Idea)
 admin.site.register(Motivation)
 admin.site.register(D_Company)
 admin.site.register(Adoption)
-admin.site.register(RegistSets, RegistSetsAdmin)
 admin.site.register(Interview)
 admin.site.register(Interviewer)

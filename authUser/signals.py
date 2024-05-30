b27f3a1e13df10480c89d14e93851635709998c2
1F10220082 <s1f102200828@iniad.org>
2024-05-30 15:11:09 +0900

    一旦修正

--- authUser/signals.py
+++ authUser/signals.py
@@ -3,9 +3,9 @@ from django.dispatch import receiver
 from .models import CustomUser
 from django.contrib.auth.hashers import make_password
 
-
+"""
 @receiver(pre_save, sender=CustomUser)
 def change_password(sender, instance, **kwargs):
     user = CustomUser.objects.get(username=instance.username)
     if instance.password != user:
-        instance.password = make_password(instance.password)
+        instance.password = make_password(instance.password)"""

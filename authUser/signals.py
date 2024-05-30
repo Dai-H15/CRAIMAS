56a34774e177dbe07cfd942f385abb0574e7e1dd
1F10220082 <s1f102200828@iniad.org>
2024-05-31 00:22:19 +0900

    緊急で修正

--- authUser/signals.py
+++ authUser/signals.py
@@ -5,17 +5,6 @@ from django.contrib.auth.hashers import make_password
 import datetime
 
 
-@receiver(pre_save, sender=CustomUser)
-def change_password(sender, instance, **kwargs):
-    try:  # ユーザーが存在する場合の処理
-        user = CustomUser.objects.get(username=instance.username)
-        if instance.password != user.password:
-            print('changed')
-            instance.password = make_password(instance.password)
-    except CustomUser.DoesNotExist:
-        pass
-
-
 @receiver(post_init, sender=CustomUser)
 def is_account_expired(sender, instance, **kwargs):
     if instance.ExpiryDate < datetime.date.today():

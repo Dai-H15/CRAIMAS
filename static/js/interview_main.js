c72922bd1df9eea3dae546838fc6cef78371d9e8
1F10220082 <s1f102200828@iniad.org>
2024-06-18 15:12:53 +0900

    URLが正しく処理されない問題を修正

--- static/js/interview_main.js
+++ static/js/interview_main.js
@@ -1,6 +1,5 @@
 let wind4;
-        function view_interview(InterviewID){
-            let d_url = "{% url 'view_interview' 'placeholder' %}";
+        function view_interview(d_url, InterviewID){
             let url = d_url.replace('placeholder', InterviewID);
             wind4 = window.open(`${url}`, "view_interview"+InterviewID, "width=500,height=700,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes");
         };

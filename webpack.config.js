71190e86a6c93320989fedf3fdf9744ab80275e3
1F10220082 <s1f102200828@iniad.org>
2024-06-19 00:27:57 +0900

    秘匿化の続き

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: `./develop_static/js/base.js`,
+  entry: `./develop-static/js/base.js`,
   output: {
     filename: "main.js",
     path: path.join(__dirname, './static/js/'),

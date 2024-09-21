7f6fe3ba82ace99cb4411bfef91511680e7ba683
1F10220082 <s1f102200828@iniad.org>
2024-09-22 01:27:35 +0900

    カレンダー機能の企業による絞り込み #49

--- task_calendar/urls.py
+++ task_calendar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("calendar_main/", views.calendar_main, name="calendar_main"),
-    path("get_calendar/<str:year>/<str:month>/<str:status>", views.get_calendar, name="get_calendar"),
+    path("get_calendar/<str:year>/<str:month>/<str:status>/<str:search>", views.get_calendar, name="get_calendar"),
     path("new_task/", views.new_task, name="new_task"),
 ]
--- task_calendar/views.py
+++ task_calendar/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from main.views import collect_regnum, collect_regsets
 import calendar
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from main.models import Interview
+from main.models import Interview, RegistSets
 
 # Create your views here.
 
 
-def get_listInterview(request, year, month, status):
+def get_listInterview(request, year, month, status, search):
+    interview = Interview.objects.filter(by_U_ID=request.user.U_ID).filter(date__year=year).filter(date__month=month)
+    if search != "search":
+        registid = RegistSets.objects.get(by_U_ID=request.user.U_ID, RegistID=search)
+        interview = interview.filter(RegistID=registid)
     if (status == "active"):
-        jsons = [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in Interview.objects.filter(by_U_ID=request.user.U_ID).filter(date__year=year).filter(date__month=month).filter(RegistID__isActive=True)]
+        jsons = [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in interview.filter(RegistID__isActive=True)]
     else:
-        jsons = [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in Interview.objects.filter(by_U_ID=request.user.U_ID).filter(date__year=year).filter(date__month=month)]
+        jsons = [{"Interview": i, "date": timezone.make_naive(i.date).day} for i in interview]
 
     return jsons
 
@@ -23,6 +27,7 @@ def calendar_main(request):
     year = timezone.make_naive(timezone.now()).year
     month = timezone.make_naive(timezone.now()).month
     today = timezone.make_naive(timezone.now()).day
+    contexts["company_list"] = collect_regsets(request.user)
     contexts["def_year"] = year
     contexts["def_month"] = month
     contexts["today"] = today
@@ -30,12 +35,15 @@ def calendar_main(request):
 
 
 @login_required
-def get_calendar(request, year, month, status):
+def get_calendar(request, year, month, status, search):
     contexts = {}
     calendar.setfirstweekday(calendar.SUNDAY)
     list_calendar = calendar.monthcalendar(int(year), int(month))
     contexts["list_calendar"] = list_calendar
-    contexts["list_interview"] = get_listInterview(request, year, month, status)
+    try:
+        contexts["list_interview"] = get_listInterview(request, year, month, status, search)
+    except (RegistSets.DoesNotExist, Interview.DoesNotExist):
+        return HttpResponse("不正な操作を検出しました。")
     if year == str(timezone.make_naive(timezone.now()).year) and month == str(timezone.make_naive(timezone.now()).month):
         contexts["today"] = timezone.make_naive(timezone.now()).day
         contexts["feature"] = False

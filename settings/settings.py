9dc50f03d3c3d312492417bd1da60dd6ce80fd91
1F10220082 <s1f102200828@iniad.org>
2024-10-15 09:52:44 +0900

    JSライブラリの更新

--- settings/settings.py
+++ settings/settings.py
@@ -92,15 +92,7 @@ DATABASES = {
         'PORT': "5432",
     }
 }
-"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
 
 
 
@@ -169,3 +161,9 @@ if DEBUG is True:
     INSTALLED_APPS.append("debug_toolbar")
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
     INTERNAL_IPS = ['127.0.0.1']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }

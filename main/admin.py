8a2125e10447869cb4d32a4169d3c962b4e7e61e
1F10220082 <s1f102200828@iniad.org>
2024-10-28 23:48:18 +0900

    更に修正

--- main/admin.py
+++ main/admin.py
@@ -15,8 +15,8 @@ class RegistSetsAdmin(admin.ModelAdmin):
 
 class InterviewAdmin(admin.ModelAdmin):
     model = Interview
-    list_display = ("title", "company_name", "InterviewID", "RegistID__RegistID")
-    search_fields = ["title", "company_name", "InterviewID", "RegistID__RegistID"]
+    list_display = ("title", "company_name", "InterviewID", "by_U_ID")
+    search_fields = ["title", "company_name", "InterviewID", "by_U_ID"]
 
 
 admin.site.register(RegistSets, RegistSetsAdmin)
